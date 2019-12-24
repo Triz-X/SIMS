b20fce023ac0dd2105375a6b246a3764364fdf3b
Triz-x <995692522@qq.com>
2019-12-24 11:52:44 +0800

    基本完成

---
+++ src/com/Triz/SIMS/util/DbUtil.java
@@ -0,0 +1,48 @@
+package com.Triz.SIMS.util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.management.loading.PrivateClassLoader;
+
+public class DbUtil {
+
+	private String dbUrl = "jdbc:mysql://localhost:3306/db_student_manager_web?useUnicode=true&serverTimezone=UTC&characterEncoding=utf8";
+	private String dbUser = "root";
+	private String dbPassword = "123456";
+	private String jdbcName = "com.mysql.cj.jdbc.Driver";
+	private Connection connection = null;
+	public Connection getConnection(){
+		try {
+			Class.forName(jdbcName);
+			connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+			System.out.println("数据库连接成功");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("数据库连接失败");
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	public void closeCon(){
+		if(connection != null)
+			try {
+				connection.close();
+				System.out.println("数据库已关闭");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbUtil dbUtil = new DbUtil();
+		dbUtil.getConnection();
+	}
+
+}
+
